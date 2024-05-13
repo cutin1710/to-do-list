5059b29aa60f88f4afdc2a9c0439231ce9cdd6d3
Gabriel Cutin <gcutin@gmail.com>
2024-05-13 19:09:24 -0300

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,11 +7,29 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
-    db.query("INSERT INTO test (nome) VALUE ('Gabriel')")
+app.post('/to-do-list', (req, res) => {
+    const name = req.body.name
+    const content = req.body.content
+
+    db.query("SELECT * FROM tasks WHERE name = ?", [name], (err, result) => {
+        if(err) {
+            res.send(err)
+        }
+        if(result.length == 0) {
+            db.query("INSERT INTO tasks (name, task) VALUES (?, ?)", [name, content], (err, response) => {
+                if(err) {
+                    res.send(err)
+                }
+
+                res.send({ msg: "Tarefa salva" })
+            })
+        } else {
+            res.send({ msg: "Insira uma tarefa não existente" })
+        }
+    })
 })
 
-const port = 3000
+const port = 3001
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
--- frontend/to-do-list/src/main/Layout.jsx
+++ frontend/to-do-list/src/main/Layout.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
 import './Layout.css'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import * as yup from 'yup'
+import Axios from 'axios'
 
 function Layout() {
+
+    const handleSubmit = (values) => {
+        Axios.post('http://localhost:3001/to-do-list', {
+            name: values.taskName,
+            content: values.taskContent
+        }).then((res) => {
+            console.log(res)
+        })
+    }
+
+    const validation = yup.object().shape({
+        taskName: yup
+        .string()
+        .required("Digite o nome da tarefa"),
+
+        taskContent: yup
+        .string()
+        .required("Digite uma tarefa")
+    })
+
     return (
-        <h1></h1>
+        <div>
+            <h1>Gerenciador de tarefas</h1>
+            <Formik initialValues={{}} onSubmit={handleSubmit} validationSchema={validation}>
+                <Form>
+                    <div className='login-form-group'>
+                        <Field name='taskName' type='text' className='form-field' placeholder='nome da tarefa' />
+                        <ErrorMessage component='span' name='taskName' className='form-error' />
+                    </div>
+
+                    <div className='login-form-group'>
+                        <Field name='taskContent' type='text' className='form-field' placeholder='tarefa' />
+                        <ErrorMessage component='span' name='taskContent' className='form-error' />
+                    </div>
+
+                    <button className='button' type='submit'>Adicionar</button>
+                </Form>
+            </Formik>
+        </div>
     )
 }
 
